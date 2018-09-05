1b619980343d5e0f5e26f502c1923b563d94b3c3
Certil Remy <certil@noukod.com>
2018-09-05 15:07:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,18 @@
 class Dog
-  def breed(breed)
-    @breed = breed
+
+  def breed=(breed)
+    @breed=breed
   end
 
- def breed
+  def breed
     @breed
   end
 
-  def name=(new_name)
-      @name = new_name
-    end
+  def name=(name)
+    @name=name
+  end
 
-    def name
-      @name
-    end
+  def name
+    @name
+  end
 end
